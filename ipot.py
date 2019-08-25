9318a0076390a1eafaed83706ccad2c763427061
johncava <jcava@asu.edu>
2019-08-24 23:41:35 -0700

    fixed issues to make it close to the algorithm

--- ipot.py
+++ ipot.py
@@ -6,8 +6,8 @@ def cost(x,y):
 def ipot(x,y, beta=0.01, K=1):
     n = len(x)
     m = len(y)
-    T = torch.ones((n,1)).mm(torch.ones(1,m))
-    sigma = (1.0/m)*torch.ones((1,m))
+    T = torch.ones((n,1)).mm(torch.ones(m,1).t())
+    sigma = (1.0/m)*torch.ones((m,1))
     C = torch.zeros(n,m)
     for i in range(n):
         for j in range(m):
@@ -16,13 +16,14 @@ def ipot(x,y, beta=0.01, K=1):
     for i in range(n):
         for j in range(m):
             A[i,j] = torch.exp(-C[i,j]*beta)
-    for t in range(1):
+    for t in range(50):
         Q = A * T
         for k in range(K):
-            delta = 1/(n*Q*sigma)
+            delta = 1/(n*Q.mm(sigma))
             sigma = 1/(m*Q.t().mm(delta))
-        #print(delta.size(),Q.size(), sigma.size())
-        T = delta.mm(sigma.mm(Q.t()))
+        # Squeeze such that [n,1] for delta and [m,1] for sigma turn to [n] and [m]
+        # such that torch.diag() can be used to turn delta and sigma into [n,n] and [m,m] matrices
+        T = torch.diag(delta.squeeze()).mm(Q).mm(torch.diag(sigma.squeeze()))
     return T
 
 n,m = 10, 9
