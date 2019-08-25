65382b82750f1a82c1ae7d21d763511d7fe09cb1
johncava <jcava@asu.edu>
2019-08-24 23:51:57 -0700

    calculate distance from trace of T and C

--- ipot.py
+++ ipot.py
@@ -21,9 +21,11 @@ def ipot(x,y, beta=0.01, K=1):
         # Squeeze such that [n,1] for delta and [m,1] for sigma turn to [n] and [m]
         # such that torch.diag() can be used to turn delta and sigma into [n,n] and [m,m] matrices
         T = torch.diag(delta.squeeze()).mm(Q).mm(torch.diag(sigma.squeeze()))
-    return T
+    return T,C
 
 n,m = 10, 9
 x = [torch.randn(1,10) for _ in range(n)]
 y = [torch.randn(1,10) for _ in range(m)]
-T = ipot(x,y)
+T,C = ipot(x,y)
+distance = torch.trace(C.t().mm(T))
+print(distance)
