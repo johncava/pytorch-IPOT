fcdc3fee50e85dfa334b212643fd5a43bb6a7db7
John Cava <jcava@asu.edu>
2021-12-04 02:40:37 -0700

    Update ipot.py
    fixed line #15. Changed A = torch.exp(-C * beta) into A = torch.exp(-C / beta)

--- ipot.py
+++ ipot.py
@@ -12,7 +12,7 @@ def ipot(x,y, beta=0.01, K=1):
     for i in range(n):
         for j in range(m):
             C[i,j] = cost(x[i],y[j])
-    A = torch.exp(-C*beta)
+    A = torch.exp(-C / beta)
     for t in range(50):
         Q = A * T
         for k in range(K):
